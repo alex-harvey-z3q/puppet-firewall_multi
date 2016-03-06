4d47a82d18205dc1ef194f8b397a680ac7d6d5d1
Alex Harvey <alex@razorconsulting.com.au>
2016-03-06 22:17:49 +1100

    Add an example using dst_range

--- tests/init.pp
+++ tests/init.pp
@@ -12,3 +12,15 @@ firewall_multi { '00100 accept on port 80':
     '4.4.4.4/24',
   ],
 }
+
+# example using dst_range instead.
+firewall_multi { '00100 accept on port 80':
+  action => 'accept',
+  dport  => 80,
+  proto  => 'tcp',
+  source => [
+    '1.1.1.1/24',
+    '2.2.2.2/24',
+  ],
+  dst_range => '3.3.3.1-3.3.3.10',
+}
