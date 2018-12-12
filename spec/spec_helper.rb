901424187c5101ca380bb23b01fe887406d69044
Alex Harvey <alexharv074@gmail.com>
2018-12-12 22:24:58 +1100

    Resolve mock_with deprecation warning
    More info in the comments in the code.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,17 @@
 # THIS FILE IS CENTRALLY MANAGED BY sync_spec.rb!
 # DO NOT EDIT IT HERE!
 
+# Without this, a warning is seen:
+#
+#   puppetlabs_spec_helper: defaults `mock_with` to `:mocha`.
+#
+# but the RSpec.configure needs to come before require
+# puppetlabs_spec_helper.
+#
+RSpec.configure do |c|
+  c.mock_with :mocha
+end
+
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet-utils'
 
