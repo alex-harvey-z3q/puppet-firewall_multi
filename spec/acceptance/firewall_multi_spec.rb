0a5e8356d7f9267bdf9f2c4d06d8a2e429ff7986
Alex Harvey <alexharv074@gmail.com>
2021-01-24 03:02:44 +1100

    Use squiggly heredoc in acceptance test

--- spec/acceptance/firewall_multi_spec.rb
+++ spec/acceptance/firewall_multi_spec.rb
@@ -3,21 +3,21 @@ require "spec_helper_acceptance"
 describe "firewall_multi" do
   context "puppet apply" do
     it "is expected to apply and be idempotent" do
-      pp = <<-EOS
-firewall_multi { '100 allow http and https access':
-  source => [
-    '10.0.10.0/24',
-    '10.0.12.0/24',
-  ],
-  destination => [
-    '10.2.0.0/24',
-    '10.3.0.0/24',
-  ],
-  dport  => [80, 443],
-  proto  => tcp,
-  action => accept,
-}
-EOS
+      pp = <<~EOS
+      firewall_multi { '100 allow http and https access':
+        source => [
+          '10.0.10.0/24',
+          '10.0.12.0/24',
+        ],
+        destination => [
+          '10.2.0.0/24',
+          '10.3.0.0/24',
+        ],
+        dport  => [80, 443],
+        proto  => tcp,
+        action => accept,
+      }
+      EOS
       apply_manifest pp, :catch_failures => true
       expect(apply_manifest(pp, :catch_failures => true).exit_code).to be_zero
     end
@@ -34,5 +34,4 @@ EOS
 
     end
   end
-
 end
